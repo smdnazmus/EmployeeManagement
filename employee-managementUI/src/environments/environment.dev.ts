26cd8a10d63e735d7bcab1f93b83fac970ced131
Nazmus Sahed <nsahed@ad.hansalog.de>
2025-07-29 08:17:32 +0200

    changing the img src in dashboard1

--- employee-managementUI/src/environments/environment.dev.ts
+++ employee-managementUI/src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiBaseUrl: 'https://employeemanagement-1-g4hb.onrender.com'
-  apiBaseUrl: 'https://localhost:7209'
+  apiBaseUrl: 'https://employeemanagement-1-g4hb.onrender.com'
+  //apiBaseUrl: 'https://localhost:7209'
 };
